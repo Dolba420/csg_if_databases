d5d3ddaf50a9f3f407cdd684c4d52ecc34163b8a
Dolba420 <151568@csgaugustinus.nl>
2021-03-24 11:09:18 +0000

    dikke php g

--- public/PHP/opdracht_2.php
+++ public/PHP/opdracht_2.php
@@ -7,9 +7,14 @@ TYP HIERONDER JOUW PHPCODE
 
 $tekst="Ik heb een bijbaantje bij de Aldi. De Aldi betaalt goed.";
 echo "$tekst";
-//$tekst=str_replace('Aldi','Albert Heijn',$tekst);
-echo '<h4>$tekst</h4>';
-
+$tekst=str_replace('Aldi','Albert Heijn',$tekst);
+echo '<h4>' . $tekst . '</h4>';
+$tekst=strstr($tekst, 'De');
+echo $tekst . "<br>";
+echo "tekst bestaat uit " . strlen($tekst) . " tekens";
+$tekst=substr($tekst,10,5);
+echo "<br>" . $tekst . "<br>";
+echo strrev($tekst).'<br>';
 /****************************
 EINDE VAN JOUW PHPCODE
 ****************************/
--- public/PHP/opdracht_3.php
+++ public/PHP/opdracht_3.php
@@ -9,9 +9,16 @@ $waarde=9/10;
 $kwad=pow($waarde,2);
 $rec=1/$kwad;
 $afgerond=round($rec,7);
+$afgekapt=floor(1000000*$rec)/1000000;
+$a = 5;
+$b = 9;
+$c = pow(pow($a,2)+pow($b,2),0.5);
+echo "C = " . round($c,3) . "<br>";
+
+
 
 echo "Het kwadraat van $waarde is $kwad.<br>
-      Het omgekeerde daarvan is (afgerond:) $afgerond<br>";
+      Het omgekeerde daarvan is (afgerond:) $afgekapt<br>";
 
 /****************************
 EINDE VAN JOUW PHPCODE
