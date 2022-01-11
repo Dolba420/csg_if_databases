1333d9f4a0fe57995b315ab32641ea0c7f1aca97
Dolba420 <151568@csgaugustinus.nl>
2022-01-11 08:50:50 +0000

    dsfaas

---
+++ public/AID/Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+
+    <link rel="stylesheet" href="css/css.css">
+
+</head>
+<body>
+<img src="css/logo.png">
+
+
+
+</body>
+</html>
---
+++ public/phpMyAdmin/tmp/twig/f7/f75c42e84cfb489d92b34e369cdb549fd7905aee0f2dbaa3fd0f1e71416b4f1c.php
@@ -0,0 +1,66 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* login/footer.twig */
+class __TwigTemplate_4f42295f07006fab0328e9decda58c3ed4f00ddaff65a9f3a141882c9cb65193 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "</div>
+";
+        // line 2
+        if ((($context["check_timeout"] ?? null) == true)) {
+            // line 3
+            echo "    </div>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "login/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  42 => 3,  40 => 2,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "login/footer.twig", "/workspace/csg_if_databases/public/phpMyAdmin/templates/login/footer.twig");
+    }
+}
