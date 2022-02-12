4c38e511e677997d8ff01ebacd0bd2bde4c0b041
Dolba420 <151568@csgaugustinus.nl>
2022-02-12 15:33:10 +0000

    tfhgh

--- public/VNR/start_spel.php
+++ public/VNR/start_spel.php
@@ -7,17 +7,32 @@ require('php/logincheck.php');
 
 </head>
 <body>
+<div id="spelerkeuze">
 <h1>Wie gooide het dichts bij de Bullseye</h1>
 <div class="startspeler" id="startspeler">
     
-    <div class="speler1" id="speler1" onclick="startspeler(1)">
+    <div class="speler1" id="speler1" onclick="startspeler(0)">
         <h1 class="spelertekst"><?php echo $_SESSION['username']; ?></h1>
     </div>
-    <div class="speler2" id="speler2" onclick="startspeler(2)">
+    <div class="speler2" id="speler2" onclick="startspeler(1)">
     <h1 class="spelertekst"><?php echo $_POST['tegenstander']?></h1>
     </div>
 </div>
+</div>
 
+<div id="spelgestart" hidden="hidden">
+    <h1 id="stand">0 - 0</h1>
+    <div class="spelers">
+        <div class="speler1spel">
+            <h1 class="score">501</h1>
+            <h2><?php echo $_SESSION['username']; ?></h2>
+        </div>
+        <div class="speler2spel">
+            <h1 class="score">501</h1>
+            <h2><?php echo $_POST['tegenstander']?></h2>
+        </div>
+    </div>
+</div>
 
 <!--
 <table>
@@ -39,8 +54,12 @@ require('php/logincheck.php');
 </table>
 -->
 <script>
+var beginspeler = null;
+var spelers = [<?php echo "'" . $_SESSION['username'] . "'" . ",'" . $_POST['tegenstander'] . "'"; ?>];
     function startspeler(speler){
-        console.log(speler);
+        document.getElementById("spelerkeuze").hidden = "hidden";
+        document.getElementById("spelgestart").hidden = "";
+        beginspeler = speler;
     }
 </script>
 
