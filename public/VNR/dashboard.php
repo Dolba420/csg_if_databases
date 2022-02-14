8f688dc90e41647fde8a002b6029d267380a66ab
Dolba420 <151568@csgaugustinus.nl>
2022-02-14 09:07:09 +0000

    dhfhdbdfhdndndfbnb

--- public/VNR/dashboard.php
+++ public/VNR/dashboard.php
@@ -15,7 +15,7 @@ require('php/logincheck.php');
 require('php/moduscontainer.php');
 ?>
 <div class="scherm">
-        <img src="foto/logo4.png" class="dashboardfoto">
+        <img src="foto/logomooi.png" class="dashboardfoto">
 </div>
 </div>
 </html>
